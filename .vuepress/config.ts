2098081b8f901c567d791e991c751baff659f662
Riny <37863122+lvr1997@users.noreply.github.com>
2026-01-04 17:08:21 +0800

    Update blog title and author in config

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -3,8 +3,8 @@ import { defineUserConfig } from "vuepress";
 import { recoTheme } from "vuepress-theme-reco";
 
 export default defineUserConfig({
-  title: "Lavrena's Blog",
-  description: "A website for record life and work.",
+  title: "I have 项目技术文档",
+  description: "# 『I have』校园二手交易平台项目技术文档",
   dest: "public",
   head: [
     ["link", { rel: "icon", href: "/favicon.ico" }],
@@ -19,22 +19,10 @@ export default defineUserConfig({
   theme: recoTheme({
     logo: "/logo.png",
     colorMode: 'light',
-    author: "Lavrena",
+    author: "Riny",
     authorAvatar: "/head.png",
     docsRepo: "https://github.com/lvr1997/lvr1997.github.io",
-    docsBranch: "gh-pages-source",
-    // commentConfig: {
-    //   type: "valine",
-    //   options: {
-    //     appId: "Q6hMeY2PSaM9FMkXetzoJoU5-gzGzoHsz",
-    //     appKey: "iLQlev5jo2Cm5pLcI0z3qhtr",
-    //     placeholder: "填写邮箱可以收到回复提醒哦！",
-    //     verify: true, // 验证码服务
-    //     // notify: true, //
-    //     recordIP: true,
-    //     // hideComments: true
-    //   },
-    // },
+    docsBranch: "gh-pages-source"
   }),
   bundler: viteBundler({
     viteOptions: {},
